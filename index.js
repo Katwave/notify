6b1dfdb7c338f1091c0148fcf3b207340b7b002a
Katwave <65369933+Katwave@users.noreply.github.com>
2021-05-31 03:03:08 +0200

    made changes to index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-// const notifyMe = () => {
-//     if ('notification' in window) {
-//         notify()
-//     }
-//     else if ()
-// }
-
-// Notification.requestPermission();
-// const notification = new Notification("New order", {
-//   icon: "./icon.png",
-//   body: "Katlego Mangoale placed a new order!",
-// });
-
-Push.create("Hello world!", {
-  body: "How's it hangin'?",
-  icon: "/icon.png",
-  // timeout: 4000,
-  onClick: function () {
-    window.focus();
-    window.open("https://www.ordapple.com");
-    this.close();
-  },
-});
-
 navigator.serviceWorker.register("./sw.js");
 Notification.requestPermission(function (result) {
   if (result === "granted") {
     navigator.serviceWorker.ready.then(function (registration) {
-      registration.showNotification("Notification with ServiceWorker");
+      // registration.showNotification("Notification with ServiceWorker");
+      Push.create("Subscription!", {
+        body: "You have subscribed!",
+        icon: "/icon.png",
+        // timeout: 4000,
+        onClick: function () {
+          // window.focus();
+          window.open("https://www.ordapple.com");
+          // this.close();
+        },
+      });
     });
+  } else if (result === "denied") {
   } else {
-    document.getElementsByClassName("notif")[0].innerHTML = "not fiound";
+    Notification.requestPermission().then((res) => {
+      Push.create("Subscription!", {
+        body: "Thank you for subscribing!",
+        icon: "/icon.png",
+        // timeout: 4000,
+        onClick: function () {
+          // window.focus();
+          window.open("https://www.ordapple.com");
+          // this.close();
+        },
+      });
+    });
   }
 });
