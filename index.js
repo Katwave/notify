f1085765c2bfa91ae9cb97e916be21357158fc49
Katwave <65369933+Katwave@users.noreply.github.com>
2021-05-31 03:52:20 +0200

    made changes to index.js and added offline.html and changes to sw.js

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-navigator.serviceWorker.register("./sw.js");
+// Register service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function (error) {
+      console.log("Service worker registration failed, error:", error);
+    });
+}
+
 Notification.requestPermission(function (result) {
   if (result === "granted") {
     navigator.serviceWorker.ready.then(function (registration) {
       // registration.showNotification("Notification with ServiceWorker");
-      Push.create("Subscription!", {
-        body: "You have subscribed!",
+      var options = {
+        body: "This notification was generated from a push!",
         icon: "/icon.png",
-        // timeout: 4000,
-        onClick: function () {
-          // window.focus();
-          window.open("https://www.ordapple.com");
-          // this.close();
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: "2",
         },
-      });
+        actions: [
+          {
+            action: "explore",
+            title: "Explore this new world",
+            icon: "/icon.png",
+          },
+          { action: "close", title: "Close", icon: "/icon.png" },
+        ],
+      };
+      registration.showNotification("Subscription!", options);
     });
   } else if (result === "denied") {
-  } else {
+  } else if (result != "denied" && result != "granted") {
     Notification.requestPermission().then((res) => {
-      Push.create("Subscription!", {
-        body: "Thank you for subscribing!",
+      var options = {
+        body: "Thank you for granting us access",
         icon: "/icon.png",
-        // timeout: 4000,
-
-        link: "https://www.ordapple.com",
-      });
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: "2",
+        },
+        actions: [
+          {
+            action: "explore",
+            title: "Explore this new world",
+            icon: "/icon.png",
+          },
+          { action: "close", title: "Close", icon: "/icon.png" },
+        ],
+      };
+      registration.showNotification("Subscription!", options);
     });
   }
 });
--- sw.js
+++ sw.js
@@ -1,22 +1,55 @@
-"use strict";
-function isFunction(obj) {
-  return obj && {}.toString.call(obj) === "[object Function]";
-}
-function runFunctionString(funcStr) {
-  if (funcStr.trim().length > 0) {
-    var func = new Function(funcStr);
-    if (isFunction(func)) {
-      func();
-    }
-  }
-}
-self.addEventListener("fetch", (e) => {});
-self.addEventListener("message", function (event) {
-  self.client = event.source;
-});
-self.onnotificationclose = function (event) {
-  runFunctionString(event.notification.data.onClose);
-  self.client.postMessage(
-    JSON.stringify({ id: event.notification.data.id, action: "close" })
+self.addEventListener("fetch", (e) => {
+  console.log("Fetching:", e.request.url);
+
+  e.respondWith(
+    caches.open("mysite-dynamic").then(function (cache) {
+      return cache
+        .match(e.request)
+        .then(function (response) {
+          return (
+            response ||
+            fetch(e.request).then(function (response) {
+              cache.put(e.request, response.clone());
+              return response;
+            })
+          );
+        })
+        .catch(() => {
+          // If both fail, show a generic fallback:
+          return caches.match("/offline.html");
+        });
+    })
   );
-};
+});
+self.addEventListener("install", function (event) {
+  // Perform some task
+  console.log("Service worker installing...");
+
+  event.waitUntil(
+    caches.open("v1-cache").then(function (cache) {
+      return cache.addAll([
+        "/index.html",
+        "/icon.png",
+        "/index.js",
+        "/offline.html",
+      ]);
+    })
+  );
+  // caches.open('example-cache').then(function(cache) {
+  //   cache.matchAll('/').then(function(response) {
+  //     response.forEach(function(element, index, array) {
+  //       cache.delete(element);
+  //     });
+  //   });
+  // })
+
+  // fetch(url).then(function (response) {
+  //   return cache.put(url, response);
+  // })
+
+  self.skipWaiting();
+});
+self.addEventListener("activate", function (event) {
+  // Perform some task
+  console.log("Service worker activating...");
+});
